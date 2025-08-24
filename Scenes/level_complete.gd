7712ac998e3ec1c32b1fd1546f4fb20043fb8494
preuss <87501978+felipepreuss@users.noreply.github.com>
2025-08-24 19:13:15 -0300

    Add level complete screen and logic
    Introduced a new level_complete.gd script and connected it to the level_complete.tscn scene, enabling level progression and returning to the main menu. Updated tutorial.gd to transition to the level complete screen. Minor adjustments to player and pistol transforms in their respective scenes.

---
+++ Scenes/level_complete.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+var levels = [
+	"res://Scenes/level-1.tscn",
+	"res://Scenes/level-2.tscn"
+]
+
+var current_level := 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+func next_level():
+	if current_level < levels.size():
+		get_tree().change_scene_to_file(levels[current_level])
+		current_level += 1
+	else:
+		get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+
+func _on_next_button_pressed() -> void:
+	next_level() # Replace with function body.
--- Script/tutorial.gd
+++ Script/tutorial.gd
@@ -9,4 +9,4 @@ func _ready():
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.name == "player" and Globals.contador == 0:
 		body.reset_powerups()
-		get_tree().change_scene_to_file("res://Scenes/level-1.tscn")
+		get_tree().change_scene_to_file("res://Scenes/level_complete.tscn")
