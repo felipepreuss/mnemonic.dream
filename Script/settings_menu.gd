7de0ea3a8e28fb2579e112bd7f73979fb3f75018
preuss <87501978+felipepreuss@users.noreply.github.com>
2025-08-25 20:22:47 -0300

    Add resolution selection to settings menu
    Connected the OptionButton's item_selected signal to a new handler in settings_menu.gd, allowing users to change the window size based on selected resolution. Also set 'gun_name' property in taco.tscn Node3D to 'Bat'.

--- Script/settings_menu.gd
+++ Script/settings_menu.gd
@@ -7,3 +7,15 @@ func _on_apply_button_pressed() -> void:
 
 func _on_exit_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+
+
+func _on_option_button_item_selected(index: int) -> void:
+	match index:
+		0:
+			DisplayServer.window_set_size(Vector2i(1152,648))
+			
+		1:
+			DisplayServer.window_set_size(Vector2i(800,600))
+		2:
+			DisplayServer.window_set_size(Vector2i(640,480))
+	
