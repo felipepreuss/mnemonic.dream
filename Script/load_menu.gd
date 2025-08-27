9a132b55872ccaea8f4fc796f05a33482c216002
Lucas L <lucas.lobo23@nave.org.br>
2025-08-26 21:54:11 -0300

    load menu WIP
    added WIP load menu

--- Script/load_menu.gd
+++ Script/load_menu.gd
@@ -7,3 +7,15 @@ func _physics_process(delta: float) -> void:
 
 func _on_texture_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+
+
+func _on_file_1_pressed() -> void:
+	print("select file 1")
+
+
+func _on_file_2_pressed() -> void:
+	print("select file 2")
+
+
+func _on_file_3_pressed() -> void:
+	print("select file 3")
