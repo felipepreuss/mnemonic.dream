748f2cd6e5e60161a148986c7434cb5f41321521
preuss <87501978+felipepreuss@users.noreply.github.com>
2025-09-03 21:42:15 -0300

    Improve gun pickup instantiation and transform
    Adjusted the Pickup node's transform in tutorial.tscn for more accurate orientation. Updated pickup.gd to set the gun visual's process mode to disabled and added comments for clarity. Minor formatting improvements and clarified player weapon assignment logic.

--- pickup.gd
+++ pickup.gd
@@ -5,18 +5,23 @@ extends Area3D
 
 func _ready():
 	animation_gun.play("rotaaate")
-	# Just instantiate the gun scene to make it visible
+	
+	# instancia a arma na cena 
 	if new_gun != null:
 		var gun_visual = new_gun.instantiate()
+	 
+		gun_visual.set_process_mode(Node.PROCESS_MODE_DISABLED)
 		add_child(gun_visual)
+
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("Player"):
-		print("foi")	
+		print("foi")
 		var weapons_manager = body.find_child("WeaponsManager")
 		
 		if weapons_manager:
 			weapons_manager.get_new_weapon(new_gun)
+			#adiciona ao player
 			queue_free()
 		else:
 			print("WeaponsManager n foi")
