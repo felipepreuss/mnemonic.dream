d442aeba78fac48fbfcafaa832e86735a70767c0
preuss <87501978+felipepreuss@users.noreply.github.com>
2025-09-12 16:04:32 -0300

    update massivo

--- Script/taco.gd
+++ Script/taco.gd
@@ -2,15 +2,19 @@ extends WeaponsManager
 
 @onready var animation_player = $"Taco de baseball/AnimationPlayer"
 @onready var hitbox = $"Taco de baseball/Area3D"
+@onready var hit: AudioStreamPlayer3D = $SeZuburi
 
 func _physics_process(delta):
 
 	if Input.is_action_pressed("Left-Click") && melee:
 		animation_player.play("attack")
 		for enemies in hitbox.get_overlapping_bodies():
+			
 			if enemies.has_method("calcularDano"):
+				
 				get_parent().get_parent().add_shake(0.5)
 				enemies.calcularDano(50)
+				hit.play()
 		if animation_player:
 			animation_player.queue("idle")
 func _on_area_3d_body_entered(body):
